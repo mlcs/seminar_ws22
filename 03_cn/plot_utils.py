74cc804c84cb20b02ab57b6f50c56cddb10f2a63
fstrnad <felix.strnad@uni-tuebingen.de>
2022-03-23 09:56:45 +0100

    Updated tutorial with examples and dendrogram.

--- 03_cn/plot_utils.py
+++ 03_cn/plot_utils.py
@@ -214,3 +214,61 @@ def plot_dendrogram(model, **kwargs):
 
     # Plot the corresponding dendrogram
     dendrogram(linkage_matrix, **kwargs)
+
+
+def fancy_dendrogram(*args, **kwargs):
+    """
+    Inspired from
+    https://joernhees.de/blog/2015/08/26/scipy-hierarchical-clustering-and-dendrogram-tutorial/
+
+    Returns
+    -------
+    Dendogramm data as scipy dataset.
+
+    """
+    from matplotlib import ticker
+
+    fig, ax = plt.subplots(figsize=(9, 9))
+    model = kwargs.pop('model', None)
+    counts = np.zeros(model.children_.shape[0])
+    n_samples = len(model.labels_)
+    for i, merge in enumerate(model.children_):
+        current_count = 0
+        for child_idx in merge:
+            if child_idx < n_samples:
+                current_count += 1  # leaf node
+            else:
+                current_count += counts[child_idx - n_samples]
+        counts[i] = current_count
+    linkage_matrix = np.column_stack(
+        [model.children_, model.distances_, counts]
+    ).astype(float)
+
+    max_d = kwargs.pop('max_d', None)
+    if max_d and 'color_threshold' not in kwargs:
+        kwargs['color_threshold'] = max_d
+    annotate_above = kwargs.pop('annotate_above', 0)
+    lw = kwargs.pop('lw', None)
+    if lw is None:
+        lw = 1
+    with plt.rc_context({'lines.linewidth': lw}):
+        ddata = dendrogram(linkage_matrix,
+                           *args, **kwargs)
+    if not kwargs.get('no_plot', False):
+
+        ax.set_xlabel('sample index (cluster size) ')
+        ax.set_ylabel('Cluster Level')
+        for i, d, c in zip(ddata['icoord'], ddata['dcoord'], ddata['color_list']):
+            x = 0.5 * sum(i[1:3])
+            y = d[1]
+            if y > annotate_above:
+                ax.plot(x, y, 'o', c=c)
+                ax.annotate("%.3g" % y, (x, y), xytext=(0, -5),
+                            textcoords='offset points',
+                            va='top', ha='center')
+        if max_d:
+            ax.axhline(y=max_d, c='k', lw=4, ls='--')
+    for axis in [ax.yaxis]:
+        axis.set_major_locator(ticker.MaxNLocator(integer=True))
+
+    return ddata
